c85f2eb7a611579753f57c9844600d4bd7e59be3
DBullHDi <ch_catalin@yahoo.com>
2015-11-22 16:53:50 +0200

    Testing git

---
+++ app.js
@@ -0,0 +1,14 @@
+/**
+ * Created by FExJohn on 11/22/2015.
+ */
+
+
+Ext.application({
+    name:'MyApp',
+    appFolder:'app',
+    controllers:[
+        'MyApp.controller.main.Main'
+
+    ],
+    autoCreateViewport: true
+});
